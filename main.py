59b2ab95bbb6cd50df68faadeeb9424252b7774d
Santiago Eduardo Poblete Talamante <133782845+santiagopoblete@users.noreply.github.com>
2024-06-17 08:47:51 +0000

    changed db made progress on main

--- main.py
+++ main.py
@@ -1,35 +1,55 @@
 import pandas as pd
 
 # Read the CSV file
-df = pd.read_csv('./archive/games.csv')
-teamNames = pd.read_csv('./archive/teams.csv')
+lineScore = pd.read_csv('./csv/line_score.csv')
+games = pd.read_csv('./csv/game.csv')
 
 # Get the column names and row count
-column_names = df.columns.tolist()
-row_count = df.shape[0]
+column_names = lineScore.columns.tolist()
+row_count = lineScore.shape[0]
 
 print(column_names, row_count)
 
-# Getting point differential for each game played
-df['point_diff'] = df['PTS_home'] - df['PTS_away']
+# Remove all rows that dont have a value in any quarter
+lineScore = lineScore.dropna(subset=['pts_qtr1_home', 'pts_qtr1_away', 'pts_qtr2_home', 'pts_qtr2_away', 'pts_qtr3_home', 'pts_qtr3_away', 'pts_qtr4_home', 'pts_qtr4_away'])
 
-# Getting the winer of the game
-df['WINNER'] = df['HOME_TEAM_WINS'].apply(lambda x: 'HOME_TEAM' if x == 1 else 'VISITOR_TEAM')
+# Get points by third quarter
+ptsQtr3Home = lineScore['pts_qtr3_home'] + lineScore['pts_qtr2_home'] + lineScore['pts_qtr1_home']
+ptsQtr3Away = lineScore['pts_qtr3_away'] + lineScore['pts_qtr2_away'] + lineScore['pts_qtr1_away']
 
-# Select relevant columns
-pointDiffTeams = df[['point_diff', 'HOME_TEAM_ID', 'VISITOR_TEAM_ID', 'WINNER', 'GAME_DATE_EST']]
+# Remove games where differential is less than 10 points
+lineScore = lineScore[abs(ptsQtr3Home - ptsQtr3Away) > 10]
 
-# Get 20 highest point differentials
-highest_point_diff = pointDiffTeams.nlargest(20, 'point_diff')
+# Get points by fourth quarter of remaining games
+ptsQtr4Home = lineScore['pts_home']
+ptsQtr4Away = lineScore['pts_away']
 
-# Replacing team IDs with team names
-highest_point_diff = highest_point_diff.replace({'HOME_TEAM_ID': teamNames.set_index('TEAM_ID')['NICKNAME']})
-highest_point_diff = highest_point_diff.replace({'VISITOR_TEAM_ID': teamNames.set_index('TEAM_ID')['NICKNAME']})
+# Get all ids ('game_id') from games where the team that was losing in the third quarter won the game
+homeWinningTeams = lineScore[(ptsQtr3Home < ptsQtr3Away) & (ptsQtr4Home > ptsQtr4Away)]
+awayWinningTeams = lineScore[(ptsQtr3Home > ptsQtr3Away) & (ptsQtr4Home < ptsQtr4Away)]
+homeWinningTeamIds = homeWinningTeams['game_id']
+awayWinningTeamsIds =  awayWinningTeams['game_id']
+winningTeamIds = pd.concat([homeWinningTeamIds, awayWinningTeamsIds])
 
-#Adding a new column to show the winning team and the year
-highest_point_diff['WINNING_TEAM'] = highest_point_diff['WINNER']
-highest_point_diff['YEAR'] = highest_point_diff['GAME_DATE_EST'].str[:4]
+# Get matching games from games dataframe
+comebackGames = games[games['game_id'].isin(winningTeamIds)]
+comebackGamesIds = comebackGames['game_id']
+print(comebackGamesIds.head(5))
+
+# remove games where no 3pt attempts were made
+comebackGames = comebackGames.dropna(subset=['fg3a_home', 'fg3a_away'])
 
 
+# Get avg 3pt attempts
+avgThreePtAttemptsHome = comebackGames['fg3a_home'].mean()
+avgThreePtAttemptsAway = comebackGames['fg3a_away'].mean()
 
-print(highest_point_diff)
+print(avgThreePtAttemptsHome, avgThreePtAttemptsAway)
+
+# Get 3pt attempts
+threePtAttemptsHome = comebackGames['fg3a_home']
+threePtAttemptsAway = comebackGames['fg3a_away']
+
+# top 5 results of 3pt percentage and 3pt made
+print(threePtAttemptsHome.head(5))
+print(threePtAttemptsAway.head(5))
